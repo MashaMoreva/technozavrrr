a161588fe5391d1e2c7168a014fbdbabbfab7d7e
Masha Moreva <mashamiskova@yandex.ru>
2023-10-16 17:06:41 +0300

    added-router

--- src/main.js
+++ src/main.js
@@ -1,13 +1,10 @@
 import Vue from "vue";
 import App from "./App.vue";
-import { message1, message2 } from "./constants.js";
-import { showAlert } from "./utils.js";
+import router from "./router";
 
 Vue.config.productionTip = false;
 
 new Vue({
+  router,
   render: (h) => h(App),
 }).$mount("#app");
-
-// showAlert(message1);
-// showAlert(message2);
