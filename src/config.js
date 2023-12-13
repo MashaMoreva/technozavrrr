faf39e53c2478be6624c635a1f37abd7da270574
Masha Moreva <mashamiskova@yandex.ru>
2023-12-13 17:17:01 +0300

    added-api-base-urls-educational-and-diploma

--- src/config.js
+++ src/config.js
@@ -1,1 +1,3 @@
-export const API_BASE_URL = "https://vue-tzr.skillbox.cc";
+export const API_BASE_URL = "https://vue-study.skillbox.cc";
+
+// export const API_BASE_URL = "https://vue-tzr.skillbox.cc";
