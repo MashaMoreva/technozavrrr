02bd29769a335b81363c3f10552b093cd105af98
Masha Moreva <mashamiskova@yandex.ru>
2023-12-13 17:24:54 +0300

    created-tabs-component

---
+++ src/components/Tabs.vue
@@ -0,0 +1,27 @@
+<template>
+  <ul class="tabs">
+    <li v-for="(tab, index) in tabs" :key="index" class="tabs__item">
+      <a
+        class="tabs__link"
+        :class="{ 'tabs__link--current': currentTab === tab.name }"
+        @click="changeTab(tab.name)"
+      >
+        {{ tab.label }}
+      </a>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  props: {
+    tabs: [],
+    currentTab: "",
+  },
+  methods: {
+    changeTab(tabName) {
+      this.$emit("tabChange", tabName);
+    },
+  },
+};
+</script>
